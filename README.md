13a2e84caf752df5585ddb803b1fabe7d40a493d
eriixon <radkovsn@gmail.com>
2017-07-28 13:35:34 -0600

    init

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # thewall
+
+# Description
+
+The console game partly based on Game of Thrones series
+
+# How to play
+
+It should be very easy. Just folow hints
+
+# Use
+
+C# and MS Studio
---
+++ thewall.tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("thewall.tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("thewall.tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0c1c9b8c-7cef-41c7-9a91-7f57d97eb8cc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
